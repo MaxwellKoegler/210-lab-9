1c125cc9d159fd2a3cafd6be40149dce25d81c00
max <max.koeg@gmail.com>
2026-09-18 20:43:58 -0700

    touchpoint 1

--- main.cpp
+++ main.cpp
@@ -4,10 +4,19 @@
 
 using namespace std;
 
+const int SIZE = 4;
+
 int main() {
+    ifstream file("matrix.txt");
 
-}
+    array<array<int, SIZE>, SIZE> matrix;
+    for(int row = 0; row < SIZE; row++) {
+        for(int col = 0; col < SIZE; col++) {
+            file >> matrix[row][col];
+        }
+    }
 
-STD::vector vectorize() {
+    
+
 
 }
