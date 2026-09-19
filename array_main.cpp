ab36780438ce191bf6df32a24bb5bb5d8b347d96
max <max.koeg@gmail.com>
2026-09-18 21:43:54 -0700

    final commit

--- array_main.cpp
+++ array_main.cpp
@@ -1,3 +1,4 @@
+// COMSC-210 | Lab 9 | Maxwell Koegler
 #include <fstream>
 #include <array>
 #include <iostream>
@@ -7,23 +8,28 @@ using namespace std;
 
 const int SIZE = 6;
 
+//this program takes a square matrix of size SIZE, transposes it, identifies if its
+//symetrical and then makes a constant matrix consisting of the element average
+//this program is the array implementation, the vector implementation is in
+//vector_main.cpp
 int main() {
-    ifstream file("matrix.txt");
+    ifstream file("matrix.txt"); //SIZE X SIZE square int matrix
 
-    array<array<int, SIZE>, SIZE> matrix;
-    for(int row = 0; row < SIZE; row++) {
+    array<array<int, SIZE>, SIZE> matrix; //2d int array
+    for(int row = 0; row < SIZE; row++) { //array population
         for(int col = 0; col < SIZE; col++) {
             file >> matrix[row][col];
         }
     }
 
-    array<array<int, SIZE>, SIZE> transpose;
-    for(int row = 0; row < SIZE; row++) {
+    array<array<int, SIZE>, SIZE> transpose; //2d int transposed array
+    for(int row = 0; row < SIZE; row++) { //array population
         for(int col = 0; col < SIZE; col++) {
             transpose[col][row] = matrix[row][col];
         }
     }
 
+    //matrix printing
     cout << "original matrix read from the file: " << endl;
     for(int row = 0; row < SIZE; row++) {
         for(int col = 0; col < SIZE; col++) {
@@ -54,7 +60,8 @@ int main() {
             total += transpose[row][col];
         }
     }
-
+    
+    //averaging and array population
     double average = static_cast<double>(total) / (SIZE*SIZE); //because the matrix is square
     array<array<double, SIZE>, SIZE> constMatrix;
 
--- vector_main.cpp
+++ vector_main.cpp
@@ -1,3 +1,4 @@
+// COMSC-210 | Lab 9 | Maxwell Koegler
 #include <fstream>
 #include <vector>
 #include <iostream>
@@ -7,23 +8,28 @@ using namespace std;
 
 const int SIZE = 6;
 
+//this program takes a square matrix of size SIZE, transposes it, identifies if its
+//symetrical and then makes a constant matrix consisting of the element average
+//this program is the vector implementation, the array implementation is in
+//array_main.cpp
 int main() {
-    ifstream file("matrix.txt");
+    ifstream file("matrix.txt"); //SIZE X SIZE square int matrix
 
-    vector<vector<int>> matrix;
-    for(int row = 0; row < SIZE; row++) {
+    vector<vector<int>> matrix(SIZE, vector<int>(SIZE)); //2d int vector 
+    for(int row = 0; row < SIZE; row++) { //population
         for(int col = 0; col < SIZE; col++) {
             file >> matrix[row][col];
         }
     }
 
-    vector<vector<int>> transpose;
-    for(int row = 0; row < SIZE; row++) {
+    vector<vector<int>> transpose(SIZE, vector<int>(SIZE)); //2d int transposed vector
+    for(int row = 0; row < SIZE; row++) { //transposing
         for(int col = 0; col < SIZE; col++) {
             transpose[col][row] = matrix[row][col];
         }
     }
 
+    //matrix printing
     cout << "original matrix read from the file: " << endl;
     for(int row = 0; row < SIZE; row++) {
         for(int col = 0; col < SIZE; col++) {
@@ -48,6 +54,7 @@ int main() {
     cout << " a symmetrical matrix" << endl << endl;
     cout << "Here is a constant double matrix filled with the average of our initial matrix: " << endl;
 
+    //average calculation
     int total = 0;
     for(int row = 0; row < SIZE; row++) {
         for(int col = 0; col < SIZE; col++) {
@@ -56,8 +63,9 @@ int main() {
     }
 
     double average = static_cast<double>(total) / (SIZE*SIZE); //because the matrix is square
-    vector<vector<double>> constMatrix;
+    vector<vector<double>> constMatrix(SIZE, vector<double>(SIZE));
 
+    //matrix population
     for(int row = 0; row < SIZE; row++) {
         fill(constMatrix.at(row).begin(), constMatrix.at(row).end(), average);
 
