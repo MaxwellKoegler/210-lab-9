b559b0f436496e32788213fa337072f4e515c6a7
max <max.koeg@gmail.com>
2026-09-18 21:02:19 -0700

    touchpoint 2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <fstream>
 #include <array>
-#include <string>
-
+#include <iostream>
+ 
 using namespace std;
 
-const int SIZE = 4;
+const int SIZE = 6;
 
 int main() {
     ifstream file("matrix.txt");
@@ -16,7 +16,35 @@ int main() {
         }
     }
 
-    
+    array<array<int, SIZE>, SIZE> transpose;
+    for(int row = 0; row < SIZE; row++) {
+        for(int col = 0; col < SIZE; col++) {
+            transpose[col][row] = matrix[row][col];
+        }
+    }
 
+    cout << "original matrix read from the file: " << endl;
 
+    for(int row = 0; row < SIZE; row++) {
+        for(int col = 0; col < SIZE; col++) {
+            cout << matrix[row][col] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "matrix transpose: " << endl;
+
+    for(int row = 0; row < SIZE; row++) {
+        for(int col = 0; col < SIZE; col++) {
+            cout << transpose[row][col] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "Therefore the matrix in question is";
+    if(matrix != transpose) {
+        cout << " not";
+    }
+    cout << " a symmetrical matrix" << endl;
+    cout << "The maximum value contained in this matrix is " << *max_element(matrix.begin(), matrix.end());
 }
